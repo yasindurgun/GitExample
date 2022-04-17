319c74d6ab25f5c8d3b3218101a461baace64a88
yasindurgun <yasin.durgun52@gmail.com>
2022-04-17 15:20:43 +0300

    Add comment to LoginDto class.

--- GitExample.API/LoginDto.cs
+++ GitExample.API/LoginDto.cs
@@ -6,5 +6,7 @@
         public string UserName { get; set; }
         public string Password { get; set; }
         public decimal Price { get; set; }
+
+        //some changes in LoginDto
     }
 }
